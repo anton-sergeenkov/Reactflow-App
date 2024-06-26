d0316bc1736d1506c7799c64f15537f9bf83e8c4
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2024-06-26 10:22:16 +0400

    Bash Script Commit

--- src/App/components/Statistics/Statistics.jsx
+++ src/App/components/Statistics/Statistics.jsx
@@ -3,6 +3,7 @@ import useStore from "../../../store/store";
 
 const Statistics = () => {
   const nodes = useStore((state) => state.nodes);
+  const edges = useStore((state) => state.edges);
 
   const nodesData = nodes.map((el) => {
     return {
@@ -12,10 +13,25 @@ const Statistics = () => {
     };
   });
 
+  const edgesData = edges.map((el) => {
+    return {
+      source: el.source,
+      sourceHandle: el.sourceHandle,
+      target: el.target,
+      targetHandle: el.targetHandle,
+      id: el.id,
+    };
+  });
+
   return (
     <div className={styles.wrapper}>
-      <pre>
+      <h4 className={styles.header}>Nodes</h4>
+      <pre className={styles.code}>
         <code>{JSON.stringify(nodesData, "", 4)}</code>
+      </pre>
+      <h4 className={styles.header}>Edges</h4>
+      <pre className={styles.code}>
+        <code>{JSON.stringify(edgesData, "", 4)}</code>
       </pre>
     </div>
   );
